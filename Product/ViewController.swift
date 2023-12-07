744f60ad8116ce02c819589cc79e81f70cb5b9c8
Artur Gurgul <artur.gurgul@redge.com>
2023-12-07 20:09:03 +0100

    add dependencies between packages

--- Product/ViewController.swift
+++ Product/ViewController.swift
@@ -6,25 +6,30 @@
 //
 
 import UIKit
+import Combine
 import SPUtils
+import SPRepositories
+import SPContract
+import SPModels
 
-struct User {
-    let id: Int
-    let name: String
-}
 
 class ViewController: UIViewController {
-
+    
+    var cancelables = Set<AnyCancellable>()
+    
+    @Injected(\.usersRepository) var usersRepository: AnyUsersRepository
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let users = [User(id: 1, name: "Artur"), User(id: 2, name: "Michał")]
-        
-        let map = users.dictionay(keyPath: \.id)
-        
-        print(map[1]?.name)
+        usersRepository
+            .getAll()
+            .catch { _ in Empty<User, Never>() }
+            .collect()
+            .sink { users in
+                let map = users.dictionay(keyPath: \.id)
+                print(map[1]?.name)
+            }.store(in: &cancelables)
     }
-
-
 }
 
